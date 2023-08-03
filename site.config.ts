9542545f6da46ef3e6b9340a1382c5016bb0acff
BilalNaseem1 <bilalnaseem1@hotmail.com>
2023-08-03 22:31:17 +0500

    first

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '550db9400dcb4ae6b6cf480097a046c4',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,16 +10,17 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  domain: 'https://efficacious-raptor-ff2.notion.site/Sample-Website-550db9400dcb4ae6b6cf480097a046c4?pvs=4',
+  author: 'Bilal Naseem',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  // description: 'Example Next.js Notion Starter Kit Site',
+  description: 'Bilal Naseems site',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  github: 'BilalNaseem1',
+  linkedin: 'bilalnaseem96',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
@@ -50,16 +51,27 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+
+  // navigationStyle: 'default'
+
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'Projects',
+      pageId: '550db9400dcb4ae6b6cf480097a046c4'
+    },
+    
+    {
+      title: 'About',
+      pageId: '93d73bef73b344529678bc478e21c649'
+    },
+    {
+      title: 'Contact',
+      pageId: '58c29615deaa4f6e8ea9f2f81edaa8f3'
+    },
+    {
+      title: 'Notes',
+      pageId: '77ee989e28f3493e8e9ac43e17d1710a'
+    }
+  ]
 })
